eb420439c911e5e782a0e1a952327b63a2e97afa
Justin Rodermond <jrode@jrode.com>
2015-07-08 11:49:50 -0400

    first commit :boat:

---
+++ README.md
@@ -0,0 +1,29 @@
+# Simple Ember Test
+
+A sample project for getting started with basic Ember syntax and conventions.
+
+## What this covers
+
+There are three major pieces to the modern Ember ecosystem:
+
+1. [Ember.js](https://github.com/emberjs/ember.js) - The framework itself.
+2. [Ember Data](https://github.com/emberjs/data) - The data modeling library.
+3. [Ember CLI](http://www.ember-cli.com/) - The utility that adds build tools, project structure, and addons. 
+
+This guide focuses on just the first one, Ember.js itself, to provide some real-world examples without bombarding newcomers with multiple libraries and tool chains.
+
+## Resources
+
+- [Ember builds](http://emberjs.com/builds/)
+- [Ember API](http://emberjs.com/api/)
+- [Todo MVC](http://todomvc.com/)
+- [Things I Wish Someone Had Told Me When I Was Learning Ember](http://landongn.com/ember/things-i-wish-someone-had-told-me-when-i-was-learning-ember-js/)
+- [Sites/companies using Ember](http://emberjs.com/ember-users/)
+- [Discourse Ember source](https://github.com/discourse/discourse/tree/master/app/assets/javascripts)
+
+## Install and Run Locally
+
+1. `git clone git@github.com:jrode/simple-ember-test.git` into your Apache root
+2. Go to [http://localhost/simple-ember-test](http://localhost/simple-ember-test)
+
+
---
+++ scripts.js
@@ -0,0 +1,163 @@
+(function() {
+
+	/*****************
+	 *  application  *
+	 *****************/
+
+	Bx = Ember.Application.create({});
+
+	/************
+	 *  router  *
+	 ************/
+
+	Bx.Router.reopen({
+		location: 'history'
+	});
+
+	Bx.Router.map(function() {
+		this.route('list', {path: '/simple-ember-test'});
+		this.route('new', {path: '/simple-ember-test/new'});
+		this.resource('thing', {path: '/simple-ember-test/:id'}, function() {
+			this.route('view');
+			this.route('edit');
+		});
+	});
+
+	/************
+	 *  routes  *
+	 ************/
+
+	Bx.ListRoute = Ember.Route.extend({
+		model: function(params) {
+			var model = Bx.ListModel.create({});
+			model.load(params);
+			return model;
+		}
+	});
+
+	Bx.ThingRoute = Ember.Route.extend({
+		model: function(params) {
+			var model = Bx.ThingModel.create({});
+			model.load(params);
+			return model;
+		}
+	});
+
+	// redirect index '/thing/:id' to '/thing/:id/view'
+	Bx.ThingIndexRoute = Ember.Route.extend({
+		redirect: function(params) {
+			this.transitionTo('thing.view');
+		}
+	});
+
+	/************
+	 *  models  *
+	 ************/
+
+	var staticThings = [
+		{
+			name: 'Blarg',
+			details: 'Blargs are really great.'
+		},
+		{
+			name: 'Foo',
+			details: 'Foo is the best blarg.'
+		},
+		{
+			name: 'Bar',
+			details: 'Often confused with foo!'
+		}
+	];
+
+	Bx.ListModel = Ember.Object.extend({
+		things: Em.A([]),
+		name: '',
+		details: '',
+
+		load: function(params) {
+
+			// could do ajax call here to load model
+			// if so, the following code would go inside the success function
+
+			var things = Em.A([]);
+
+			staticThings.forEach(function(thing, i) {
+				thing.id = i;
+				things.push(Bx.ThingModel.create(thing));
+			});
+
+			this.set('things', things);
+
+		}
+	});
+
+	Bx.ThingModel = Ember.Object.extend({
+		load: function(params) {
+
+			this.set('id', params.id);
+
+			var thing = staticThings[params.id];
+
+			for (var prop in thing) {
+				this.set(prop, thing[prop]);
+			}
+		},
+
+		save: function() {
+			var self = this;
+			var id = this.get('id');
+
+			['name', 'details'].forEach(function(prop) {
+				staticThings[id][prop] = self.get(prop);
+			});
+		}
+	});
+
+	/*****************
+	 *  controllers  *
+	 *****************/
+
+	Bx.ApplicationController = Ember.Controller.extend({
+		isListPage: function() {
+			return this.currentRouteName === 'list';
+		}.property('currentRouteName')
+	});
+
+	Bx.ListController = Ember.Controller.extend({
+		actions: {
+			deleteThing: function(id) {
+				staticThings.splice(id, 1);
+				this.get('model').load();
+			}
+		}
+	})
+
+	Bx.NewController = Ember.Controller.extend({
+		actions: {
+			saveNew: function() {
+				staticThings.push({
+					name: this.get('name'),
+					details: this.get('details')
+				});
+
+				this.transitionTo('list');
+			}
+		}
+	});
+
+	Bx.ThingEditController = Ember.Controller.extend({
+		actions: {
+			saveChanges: function() {
+
+				// validate first?
+				
+				this.get('model').save();
+
+				this.transitionTo('thing.view');
+			}
+		}
+	});
+
+})();
+
+
